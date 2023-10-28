ed108b351a33396604623fbd053981e00a1e51d3
Konstantin Herud <konstantin.herud@gmail.com>
2023-10-28 23:49:02 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-cmake -DCMAKE_PREFIX_PATH=/home/konstantin/.local/grpc -DLLAMA_CUBLAS=ON ..
+cmake -DCMAKE_PREFIX_PATH=/home/konstantin/.local/grpc -DLLAMA_CUBLAS=ON -DLLAMA_BUILD_EXAMPLES=ON ..
 cmake --build . --config Release
 cd ..
